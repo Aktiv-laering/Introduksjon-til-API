2d12fad7c2fc96063055c3d48f77163fbe7ab502
Egil Skjelbred <egil@skjelbred-design.no>
2022-04-24 22:29:22 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,27 +1,61 @@
-const url = "https://randomuser.me/api/?nat=no";
-const container = document.querySelector(".container");
+/* 
 
-async function getApi() {
-    const response = await fetch(url);
-    const result = await response.json();
 
-    console.log(result.results);
+Denne koden henter inn EN person fra API'et til randomuser
 
-    // ${JSON.stringify(result.results)}
 
-    container.innerHTML = `
-    
-    <div>
-        <img src=${result.results[0].picture.large} height="200"/>
-        <h1>Navn: ${result.results[0].name.first} ${result.results[0].name.last}</h1>
-        <h2>${result.results[0].gender}</h2>
-        <h3>${result.results[0].phone}</h3>
-        <h3>${result.results[0].email}</h3>
-        <h4>${result.results[0].location.street.name} ${result.results[0].location.street.number}, ${result.results[0].location.postcode} ${result.results[0].location.state}</h4>
-    </div>
-    
-    
-    `;
-}
+*/
 
-getApi();
+// const url = "https://randomuser.me/api";
+// const container = document.querySelector(".container");
+
+// async function getApi() {
+//     const response = await fetch(url);
+//     const svar = await response.json();
+
+//     console.log(svar.results[0]);
+
+//     container.innerHTML = `
+
+//     <div>
+//         <img src=${svar.results[0].picture.large} height="200"/>
+//         <h1>${svar.results[0].name.first} ${svar.results[0].name.last}</h1>
+//         <h3>${svar.results[0].email}</h3>
+//     </div>
+
+//     `;
+// }
+
+// getApi();
+
+/* 
+
+
+Hente ut 10 personer fra API'et til nokeynoshade 
+
+
+*/
+
+// const url = "http://www.nokeynoshade.party/api/queens?limit=50";
+
+// const resultContainer = document.querySelector(".container");
+
+// async function getProducts() {
+//     const response = await fetch(url);
+//     const svar = await response.json();
+
+//     console.log(svar);
+
+//     resultContainer.innerHTML = "";
+
+//     for (let i = 0; i < 10; i++) {
+//         console.log(svar[i].name);
+//         resultContainer.innerHTML += `
+//         <img src="${svar[i].image_url}" height="200" alt="" />
+//         <h1>${svar[i].name}</h1>
+//         <p>${svar[i].quote}</p>
+//         `;
+//     }
+// }
+
+// getProducts();
